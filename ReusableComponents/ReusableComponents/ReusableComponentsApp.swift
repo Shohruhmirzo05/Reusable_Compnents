d2de2be59e74b5ddc315da03df13f7b99754e600
Shohruhmirzo1 <shohruhmirzoalijonov@gmail.com>
2024-11-14 15:09:47 +0500

    first save

---
+++ ReusableComponents/ReusableComponents/ReusableComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableComponentsApp.swift
+//  ReusableComponents
+//
+//  Created by Shohruhmirzo Alijonov on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ReusableComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
